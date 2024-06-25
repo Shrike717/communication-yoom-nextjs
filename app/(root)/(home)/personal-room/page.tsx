68534f8418ef3c400a022469765f1537ee8ab5ec
Daniel Bauer <dan717@gmx.de>
2024-06-25 16:57:12 +0200

    Sidebar.tsx
    - Added the component
    - Added detailed comments explaining component functionality and styling.
    - Implemented usePathname hook to determine active sidebar links.
    - Utilized Tailwind CSS for responsive design and layout adjustments.
    - Enhanced navigation clarity with conditional styling for active links.

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home: React.FC = () => {
+const PersonalRoom = () => {
 	return (
 		<section className='flex h-full w-full flex-col gap-10 text-white'>
-			<h1 className='text-3xl font-bold'>Home</h1>
+			<h1 className='text-3xl font-bold'>Personal Room</h1>
 		</section>
 	);
 };
 
-export default Home;
+export default PersonalRoom;
---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+// Hier liegen Daten Objeke, über die wir dann in anderen Komponenten mappen können.
+
+// Array für Sidebar:
+export const sidebarLinks = [
+	{
+		imgURL: '/icons/Home.svg',
+		route: '/',
+		label: 'Home',
+	},
+
+	{
+		imgURL: '/icons/upcoming.svg',
+		route: '/upcoming',
+		label: 'Upcoming',
+	},
+	{
+		imgURL: '/icons/previous.svg',
+		route: '/previous',
+		label: 'Previous',
+	},
+	{
+		imgURL: '/icons/Video.svg',
+		route: '/recordings',
+		label: 'Recordings',
+	},
+	{
+		imgURL: '/icons/add-personal.svg',
+		route: '/personal-room',
+		label: 'Personal Room',
+	},
+];
